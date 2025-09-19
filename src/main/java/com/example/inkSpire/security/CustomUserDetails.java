e8a6b8d5a6abbaa298fcf54008d0ba6223544ad2
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-09-19 12:54:15 +0530

    custom use details service added

---
+++ src/main/java/com/example/inkSpire/security/CustomUserDetails.java
@@ -0,0 +1,24 @@
+package com.example.inkSpire.security;
+
+import com.example.inkSpire.repository.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetails implements UserDetailsService {
+
+    private final AppUserRepository userRepository;
+
+    public CustomUserDetails(AppUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow();
+
+    }
+}
