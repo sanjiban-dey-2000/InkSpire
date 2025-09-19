0862988688147ed8cccbd93c7f40a9481befe22f
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-09-19 11:10:39 +0530

    Initial setup done

---
+++ src/main/java/com/example/inkSpire/InkSpireApplication.java
@@ -0,0 +1,13 @@
+package com.example.inkSpire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkSpireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkSpireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inkSpire/InkSpireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inkSpire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkSpireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
