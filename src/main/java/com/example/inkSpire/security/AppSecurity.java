a3ca0685e9fd7a402cfeeb11491f53d6a49a9981
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-09-20 11:55:17 +0530

    jwt filter added in the filter chain

--- src/main/java/com/example/inkSpire/security/AppSecurity.java
+++ src/main/java/com/example/inkSpire/security/AppSecurity.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class AppSecurity {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public AppSecurity(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -17,7 +24,8 @@ public class AppSecurity {
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
