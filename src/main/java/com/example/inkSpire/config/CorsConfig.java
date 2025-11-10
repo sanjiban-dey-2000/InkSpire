b8abe0519a9a4b9a88ee8eeb924655d51865e45d
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-11-10 12:57:06 +0530

    changes again in cors config

--- src/main/java/com/example/inkSpire/config/CorsConfig.java
+++ src/main/java/com/example/inkSpire/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("https://inkspire-frontend-git-main-sanjiban-deys-projects.vercel.app")
+                        .allowedOrigins("https://inkspire-frontend-lemon.vercel.app")
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
